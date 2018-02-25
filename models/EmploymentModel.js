a156fee5d9ae24887b11c002c0abb31b105a8c13
Joker <lpinzhou@163.com>
2018-02-25 20:31:35 +0800

    update the eplist and collection list

--- models/EmploymentModel.js
+++ models/EmploymentModel.js
@@ -62,13 +62,13 @@ module.exports={
             
             if(req.query['business']==undefined){
                 var countSql='select count(tid) from employment'
-                var listSql = 'select tid,title,type,place,year,month,day,hour,minute,updtime,createtime from employment order by createtime desc limit ?,?';  
+                var listSql = 'select tid,title,type,place,year,month,day,hour,minute,updtime,createtime,business from employment order by createtime desc limit ?,?';  
                 var param = [pointStart,pageSize];
             }else{
                 //console.log(req.query['business'])
-                console.log(req.query['business'])
+                //console.log(req.query['business'])
                 var countSql='select count(tid) from employment where business ="'+req.query['business']+'"'
-                var listSql = 'select tid,title,type,place,year,month,day,hour,minute,updtime,createtime from employment where business="'+req.query['business']+'" order by createtime desc limit ?,?';  
+                var listSql = 'select tid,title,type,place,year,month,day,hour,minute,updtime,createtime,business from employment where business="'+req.query['business']+'" order by createtime desc limit ?,?';  
                 var param = [pointStart,pageSize];
                 
             }
@@ -98,18 +98,18 @@ module.exports={
                 // count=(results['one'][0]['count(*)'])
                 // countPage=Math.ceil(count/pageSize)
                 rs=(results['two'])
-                console.log(rs)
+                //console.log(rs)
                 //res.render('zpindex', { title: 'Express',loginbean:loginbean,rs:rs,count:count,countPage:countPage,page:page});
                 if(req.session.loginbean==undefined){
-                    res.render('zpindex', { title: 'Express',loginbean:loginbean,rs:rs,count:count,countPage:countPage,page:page});
+                    res.render('zpindex', { title: 'Express',loginbean:loginbean,rs:rs,count:count,countPage:countPage,page:page,business:req.query['business']});
                 }else{
                     if(req.session.loginbean.privilige==1){
-                        res.render('zpindex1', { title: 'Express',loginbean:loginbean,rs:rs,count:count,countPage:countPage,page:page});
+                        res.render('zpindex1', { title: 'Express',loginbean:loginbean,rs:rs,count:count,countPage:countPage,page:page,business:req.query['business']});
                     }else{
-                        res.render('zpindex', { title: 'Express',loginbean:loginbean,rs:rs,count:count,countPage:countPage,page:page});
+                        res.render('zpindex', { title: 'Express',loginbean:loginbean,rs:rs,count:count,countPage:countPage,page:page,business:req.query['business']});
                     }
                 }
-                
+                //console.logconsole.log("business:"+req.query['business'])
                 //res.send("aaaa")
             })
             
@@ -137,22 +137,68 @@ module.exports={
             res.send("没传入tid")
         }
     },
-    epList:function(req,res){
+    epList:function(req,res){//发帖列表
         uid=req.session.loginbean.id
-        console.log(uid)
+        loginbean = req.session.loginbean
+        //console.log(uid)
         //res.send('发帖列表')
         pool=connPool();
         pool.getConnection(function(err,conn){
             if(err){
                 res.send('获取链接错误，错误原因：'+err.message);
             }
-            var sqllist='select tid,title,createtime from employment where uid=? order by tid desc'
-            var param=[uid]
-            conn.query(sqllist,param,function(err,rs){
-                    console.log(rs[0])
-                    //res.send('发帖列表')        
-                    res.render('zpeplist',{rs:rs})
-            })
+            page=1;
+            if(req.query['page']!=undefined){
+                page=parseInt(req.query['page']);
+                if(page<1){
+                    page=1;
+                }
+            }
+            pageSize=10;//每页显示多少条帖子
+            pointStart=(page-1)*pageSize;
+            count=0;//****************** */
+            countPage=0;//************* */
+
+            if(req.query['business']==undefined){
+                var countSql='select count(tid) from employment where uid=?'
+                var countparam=[uid]
+                var sqllist='select tid,title,type,place,year,month,day,hour,minute,updtime,createtime from employment where uid=? order by tid desc limit ?,?'
+                var listparam=[uid,pointStart,pageSize]
+            }else{
+                var countSql='select count(tid) from employment where uid=? and business ="'+req.query['business']+'"'
+                var countparam=[uid]
+                var sqllist='select tid,title,type,place,year,month,day,hour,minute,updtime,createtime from employment where uid=? and business ="'+req.query['business']+'" order by tid desc limit ?,?'
+                var listparam=[uid,pointStart,pageSize]
+            }
+
+            async.series({
+                one:function(callback){
+                    conn.query(countSql,countparam,function(err,rs){ 
+                        count=rs[0]['count(tid)']
+                        countPage=Math.ceil(count/pageSize)
+                        //console.log("countPage:"+countPage)
+                        //console.log("page:"+page)
+                        if(page>countPage){
+                            page=countPage
+                            //console.log("page1:"+page)
+                            pointStart=(page-1)*pageSize;
+
+                        }
+                       // console.log("count:"+count)
+                       listparam = [uid,pointStart,pageSize]
+                        callback(null,rs)
+                    })   
+                },
+                two:function(callback){
+                    conn.query(sqllist,listparam,function(err,rs){   
+                        callback(null,rs)  
+                    }) 
+                }
+            },function(err,results) {
+                rs=(results['two'])
+                res.render('zpeplist',{title:'Express',loginbean:loginbean,rs:rs,count:count,countPage:countPage,page:page,business:req.query['business'],web:1})
+            })   
+
             conn.release();
         })
     },
@@ -170,33 +216,35 @@ module.exports={
             var sqlparam=[uid,tid];
             var colupdate='update collection set rank=? where uid=? and tid=?'
             var updparam=[rank,uid,tid]
-            var colinsert='insert into collection(uid,tid,rank,title) values(?,?,?,?)'
-            var insertparam=[uid,tid,rank,title]
+            var colinsert='insert into collection(uid,tid,rank,title,type,year,month,day,hour,minute,place,business) values(?,?,?,?,?,?,?,?,?,?,?,?)'
+            var insertparam=[uid,tid,rank,title,req.query['type'],req.query['year'],req.query['month'],req.query['day'],req.query['hour'],req.query['minute'],req.query['place'],req.query['business']]
             conn.query(colsql,sqlparam,function(err,rs){
                 if(err){
                     res.send('数据库错误，错误原因'+err.message)
                     return;
                 }
                 if(rs.length>0){
-                    console.log('数据表存在记录'+'uid='+uid+';tid='+tid)
+                    // console.logconsole.log('数据表存在记录'+'uid='+uid+';tid='+tid)
                     conn.query(colupdate,updparam,function(err,rs){
                         if(err){
                             res.send('数据库错误，错误原因：'+err.message)
                             return
                         }
-                        console.log(rs)
+                        // console.log(rs)
                     })
                     res.redirect('/?page='+req.query['page'])
                     //res.send('数据表存在记录')
                 }
                 else{
-                    console.log('数据表不存在记录'+'uid='+uid+';tid='+tid)
+                    // console.log('数据表不存在记录'+'uid='+uid+';tid='+tid)
                     conn.query(colinsert,insertparam,function(err,rs){
                         if(err){
+                            // console.log('数据库错误，错误原因：'+err.message)
                             res.send('数据库错误，错误原因：'+err.message)
+                           
                             return
                         }
-                        console.log(rs)
+                        // console.log(rs)
                     })
                     res.redirect('/?page='+req.query['page'])
                     //res.send('数据表不存在记录')
@@ -207,6 +255,7 @@ module.exports={
     },
     collectionList:function(req,res){
         uid=req.session.loginbean.id
+        loginbean = req.session.loginbean
         //console.log(uid)
         //res.send('发帖列表')
         pool=connPool();
@@ -214,12 +263,54 @@ module.exports={
             if(err){
                 res.send('获取链接错误，错误原因：'+err.message);
             }
-            var sqllist='select tid,title,createtime from collection where uid=? order by tid desc'
-            var param=[uid]
-            conn.query(sqllist,param,function(err,rs){
-                    console.log(rs[0])
-                    //res.send('发帖列表')        
-                    res.render('zpeplist',{rs:rs})
+            page=1;
+            if(req.query['page']!=undefined){
+                page=parseInt(req.query['page']);
+                if(page<1){
+                    page=1;
+                }
+            }
+            pageSize=10;//每页显示多少条帖子
+            pointStart=(page-1)*pageSize;
+            count=0;//****************** */
+            countPage=0;//************* */
+
+            if(req.query['business']==undefined){
+                var countSql = 'select count(cid) from collection where uid=?'
+                var countparam=[uid]
+                var sqllist='select tid,title,type,year,month,day,hour,minute,place,createtime from collection where uid=? order by tid desc limit ?,?'
+                var listparam=[uid,pointStart,pageSize]
+            }else{
+                var countSql = 'select count(cid) from collection where uid=? and business = "'+req.query['business']+'"'
+                
+                var countparam=[uid]
+                var sqllist='select tid,title,type,year,month,day,hour,minute,place,createtime from collection where uid=? and business="'+req.query['business']+'" order by tid desc limit ?,?'
+                var listparam=[uid,pointStart,pageSize]
+            }   
+            
+            async.series({
+                one:function(callback){
+                    conn.query(countSql,countparam,function(err,rs){
+                        count=rs[0]['count(cid)']
+                        // console.log("count:"+count)
+                        countPage=Math.ceil(count/pageSize)
+                        if(page>countPage){
+                            page=countPage
+                            // console.log("page1:"+page)
+                            pointStart=(page-1)*pageSize;
+                        }
+                        listparam = [uid,pointStart,pageSize]
+                        callback(null,rs)
+                    })
+                },
+                two:function(callback){
+                    conn.query(sqllist,listparam,function(err,rs){ 
+                        callback(null,rs)
+                    })
+                }
+            },function(err,results){
+                rs=(results['two'])
+                res.render('zpeplist',{title:'Express',loginbean:loginbean,rs:rs,count:count,countPage:countPage,page:page,business:req.query['business'],web:2})
             })
             conn.release();
         })
