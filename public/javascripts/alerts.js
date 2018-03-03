a12cc04518d8bd0d98f9aaee44b2762f3bf267b8
Joker <lpinzhou@163.com>
2018-03-03 13:38:33 +0800

    black_star

--- public/javascripts/alerts.js
+++ public/javascripts/alerts.js
@@ -1,11 +1,10 @@
-﻿function black_start(){
+﻿function black_star(){
   alert("需登录后，才能执行收藏功能")
 }
 
 function cancel_collection(){
 	var r=confirm("确认取消收藏？");
 	if(r==true){
-		location.href='/employment/collection/del?uid=<%= loginbean.id %>&tid=<%=row['tid']%>&page=<%=page %>&back=4';
 	}else{
 		
 	}
